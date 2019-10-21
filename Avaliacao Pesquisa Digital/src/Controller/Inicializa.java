20ebf0a4d88a1be3e7c642c30d33d07d1317389a
Cássio Gamarra <cassio1515@hotmail.com>
2019-10-20 22:56:06 -0300

    Update

---
+++ Avaliacao Pesquisa Digital/src/Controller/Inicializa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.ViewMinima;
+
+/**
+ *
+ * @author cassio
+ */
+public class Inicializa {
+    public static void main(String[] args) {
+        ViewMinima view = new ViewMinima();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+}
